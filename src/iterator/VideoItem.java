ce3f29529c8274671081c2c97b2da5860a0de975
davidluiz91 <dvdluiz1991@gmail.com>
2023-11-27 18:29:45 -0300

    primeiro commit

---
+++ src/iterator/VideoItem.java
@@ -0,0 +1,16 @@
+package iterator;
+
+public class VideoItem {
+	String nome;
+	double preco;
+	
+	VideoItem(String nome, double d){
+		this.nome = nome;
+		this.preco = d;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+}
